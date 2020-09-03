8d8008985c753301c5aa9d7e2347a7b7e8e0df7a
João Pedro Harbs <harbsprog@gmail.com>
2020-09-03 20:40:35 -0300

    Ultimos ajustes

--- app/Services/Services/ServicesService.php
+++ app/Services/Services/ServicesService.php
@@ -70,16 +70,7 @@ class ServicesService
         } else {
             try {
 
-                if ($request->hasFile('file')) {
-                    $filenameWithExt = $request->file('file')->getClientOriginalName();
-                    $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
-                    $extension = $request->file('file')->getClientOriginalExtension();
-
-                    $fileNameToStore = $filename . '_' . time() . '.' . $extension;
-
-                    $path = $request->file('file')->storeAs('images/services/', $fileNameToStore);
-                    $request['icon'] = 'storage/images/services/' . $fileNameToStore;
-                } else {
+                if (!$request->get('icon')) {
                     $request['icon'] = 'noImage';
                 }
 
